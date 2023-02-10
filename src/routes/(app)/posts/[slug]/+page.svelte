c76463eb921361983613d9da9678d001c897c94c
3200pro <68202671+3200pro@users.noreply.github.com>
2023-02-10 10:30:23 -0600

    Conditional Images & Name Removal

--- src/routes/(app)/posts/[slug]/+page.svelte
+++ src/routes/(app)/posts/[slug]/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-  import { previewSubscription } from "$lib/config/sanity";
-  import { postQuery } from "$lib/config/sanity/queries";
-  import { urlForImage } from "$lib/config/sanity";
-  import type { PageData } from "./$types";
+  import { previewSubscription } from '$lib/config/sanity';
+  import { postQuery } from '$lib/config/sanity/queries';
+  import { urlForImage } from '$lib/config/sanity';
+  import type { PageData } from './$types';
 
   export let data: PageData;
 
@@ -15,30 +15,43 @@
 </script>
 
 <svelte:head>
-  <title>{$postData?.post?.title || "Post"}</title>
+  <title>{$postData?.post?.title || 'Post'}</title>
 </svelte:head>
 
 {#if $postData?.post}
-  <div class="relative w-full bg-black min-h-screen px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28">
+  <div
+    class="relative w-full bg-black min-h-screen px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-24 lg:pb-28"
+  >
     <div class="relative mx-auto max-w-2xl">
       <div class="w-full space-y-8">
         <div>
-          <a href="/" class="hover:underline font-bold text-sm text-white">← Back to home</a>
+          <a href="/" class="hover:underline font-bold text-sm text-white"
+            >← Back to home</a
+          >
         </div>
 
         <div>
           <h1 class="text-4xl font-bold">{$postData.post.title}</h1>
           <div class="mt-3 flex items-center">
-            <div class="flex-shrink-0">
-              <span class="sr-only">Roel Aufderehar</span>
-              <img
-                class="h-10 w-10 rounded-full"
-                src={urlForImage($postData.post.author.picture).crop("focalpoint").width(256).height(256).url()}
-                alt=""
-              />
-            </div>
+            {#if $postData.post.author.picture}
+              <div class="flex-shrink-0">
+                <span class="sr-only">Image</span>
+
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src={urlForImage($postData.post.author.picture)
+                    .crop('focalpoint')
+                    .width(256)
+                    .height(256)
+                    .url()}
+                  alt=""
+                />
+              </div>
+            {/if}
             <div class="ml-3">
-              <p class="text-sm font-medium text-white">{$postData.post.author.name}</p>
+              <p class="text-sm font-medium text-white">
+                {$postData.post.author.name}
+              </p>
               <div class="flex space-x-1 text-sm text-gray-400">
                 <time datetime="2020-03-16">
                   {new Date($postData.post.date).toLocaleDateString()}
@@ -47,13 +60,17 @@
             </div>
           </div>
         </div>
-
-        <img
-          class="h-92 w-full object-cover rounded-xl mb-10"
-          src={urlForImage($postData.post.coverImage).crop("focalpoint").width(1344).height(736).url()}
-          alt=""
-        />
-
+        {#if $postData.post.coverImage}
+          <img
+            class="h-92 w-full object-cover rounded-xl mb-10"
+            src={urlForImage($postData.post.coverImage)
+              .crop('focalpoint')
+              .width(1344)
+              .height(736)
+              .url()}
+            alt=""
+          />
+        {/if}
         <div>
           {$postData.post.postContent}
         </div>
